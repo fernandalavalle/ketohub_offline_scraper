bab0597a930e1f5d4d10fcf78ae22081e8063a13
Michael Lynch <michael@ketohub.io>
2017-10-15 14:07:52 -0400

    Adding support for ketoconnect ingredients

--- scraper/ketoconnect.py
+++ scraper/ketoconnect.py
@@ -6,11 +6,25 @@ def parse_recipe(metadata, response):
     title = titles.canonicalize(title_raw.split('|')[0].strip())
     category_raw = _category_from_url(metadata['referer'])
     category = _canonicalize_category(category_raw)
-    return {'title': title, 'url': metadata['url'], 'category': category}
+    ingredients_list = _parse_ingredients(response)
+    return {
+        'title': title,
+        'url': metadata['url'],
+        'category': category,
+        'ingredients': ingredients_list
+    }
 
 
 def _category_from_url(url):
     return [str(x) for x in url.split('/') if x][-1]
+
+
+def _parse_ingredients(response):
+    ingredients_raw = []
+    for ingredient_raw in response.xpath(
+            '//span[@class="wpurp-recipe-ingredient-name"]/text()').extract():
+        ingredients_raw.append(ingredient_raw)
+    return ingredients_raw
 
 
 def _canonicalize_category(category):
