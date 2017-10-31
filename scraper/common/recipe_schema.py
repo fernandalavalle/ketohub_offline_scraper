d2725ec74a071d357c30cc0097bf284ea5fdb72e
Michael Lynch <michael@ketohub.io>
2017-10-31 19:54:00 -0400

    Fixing variable name

--- scraper/common/recipe_schema.py
+++ scraper/common/recipe_schema.py
@@ -6,7 +6,7 @@ import errors
 def read(response):
     for raw_json in response.xpath(
             '//script[@type="application/ld+json"]/text()').extract():
-        json_dict = json.loads(recipe_schema_raw)
+        json_dict = json.loads(raw_json)
         if '@type' in json_dict and json_dict['@type'] == 'Recipe':
             return json_dict
 
