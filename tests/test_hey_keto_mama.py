dcf26ef7df4e506cf19592b36443942b3af3cc2e
Michael Lynch <michael@ketohub.io>
2017-12-01 22:35:30 -0500

    Adding test for Hey Keto Mama title parsing

---
+++ tests/test_hey_keto_mama.py
@@ -0,0 +1,17 @@
+import unittest
+
+from scrapy import http
+
+from scraper import hey_keto_mama
+
+
+class HeyKetoMamaParseTitleTest(unittest.TestCase):
+
+    def test_strips_trailing_page_title(self):
+        self.assertEqual(
+            hey_keto_mama.parse_title(
+                http.TextResponse(
+                    url='',
+                    body="""
+<meta property="og:title" content="Cream Cheese &amp; Salami Keto Pinwheels - Hey Keto Mama" />"""
+                )), u'Cream Cheese and Salami Keto Pinwheels')
